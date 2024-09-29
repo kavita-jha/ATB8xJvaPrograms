3776de7d6263637ce7c852890931b122051c1ccd
kavita_jha <kavita100219@gmail.com>
2024-09-29 14:30:46 +0530

    first program

--- src/sept_package/lab_002.java
+++ src/sept_package/lab_002.java
@@ -1,4 +1,13 @@
 package sept_package;
 
+
+import com.sun.source.doctree.SystemPropertyTree;
+
 public class lab_002 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=12;
+        System.out.println(a);
+        System.out.println(b);
+    }
 }
